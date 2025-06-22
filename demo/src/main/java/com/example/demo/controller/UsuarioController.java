ff4d2f8374934b68f7cdc095ed5542fc532f31ce
Meinny <c.villablanca.developer@gmail.com>
2025-06-22 02:05:46 -0400

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarUsuario(@PathVariable Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
